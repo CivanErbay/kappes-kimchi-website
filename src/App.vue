5330e0a064994df14d18206bb68895b3b71fe03e
Civan <Erbay.Civan.Extern@swm.de>
2022-12-24 18:08:35 +0100

    Init basic page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Logo from "./components/Logo.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="KAPPES KIMCHI" />
+      
+      <Logo msg="kappeskimchi" />
     </div>
   </header>
 
-  <main>
-  </main>
+  <main></main>
 </template>
 
 <style scoped>
@@ -20,20 +18,10 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 @media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
+    /* place-items: center; */
   }
 
   header .wrapper {
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,36 +2,41 @@
 defineProps({
   msg: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+  <div class="logo green">
+    <h2>김치</h2>
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <style scoped>
+.logo {
+  display: flex;
+}
+
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
-  top: -10px;
 }
 
-h3 {
-  font-size: 1.2rem;
+h2 {
+  font-size: 2.2rem;
+  font-weight: bold;
 }
 
-.greetings h1,
-.greetings h3 {
+.logo h1,
+.logo h3 {
   text-align: center;
 }
 
 @media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
+  .logo h1,
+  .logo h3 {
     text-align: left;
   }
 }
